19fb5272e4efc97a1cbc6cfeb5d9ddf86fa1756a
Ahmed Zaman <ahmedrzaman@gmail.com>
2020-01-14 17:41:17 +0000

    ImageHelper tests

---
+++ Snake/Content/ImageHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SnakeNet.GameObjects;
+
+namespace SnakeNet.Content
+{
+    public static class ImageHelper
+    {
+        /* 
+         * |╔|═|╗|
+         * |║| |║|
+         * |╚|═|╝|
+         */
+
+        public static string GetImage(MoveDirection previous, MoveDirection next)
+        {
+            if (previous == MoveDirection.Up && (next == MoveDirection.None || next == MoveDirection.Up))
+                return Images.SnakeBodyUp;
+            else if (previous == MoveDirection.Down && (next == MoveDirection.None || next == MoveDirection.Down))
+                return Images.SnakeBodyDown;
+            else if (previous == MoveDirection.Left && (next == MoveDirection.None || next == MoveDirection.Left))
+                return Images.SnakeBodyLeft;
+            else if (previous == MoveDirection.Right && (next == MoveDirection.None || next == MoveDirection.Right))
+                return Images.SnakeBodyRight;
+
+            // ╝
+            else if ((previous == MoveDirection.Right && next == MoveDirection.Up) ||
+                (previous == MoveDirection.Down && next == MoveDirection.Left))
+            {
+                return Images.SnakeBodyUpLeft;
+            }
+            // ╚
+            else if ((previous == MoveDirection.Left && next == MoveDirection.Up) || 
+                (previous == MoveDirection.Down && next == MoveDirection.Right))
+            {
+                return Images.SnakeBodyUpRight;
+            }
+            // ╔
+            else if ((previous == MoveDirection.Left && next == MoveDirection.Down) ||
+                (previous == MoveDirection.Up && next == MoveDirection.Right))
+            {
+                return Images.SnakeBodyDownRight;
+            }
+            // ╗
+            else if ((previous == MoveDirection.Right && next == MoveDirection.Down) ||
+                (previous == MoveDirection.Up && next == MoveDirection.Left))
+            {
+                return Images.SnakeBodyDownLeft;
+            }
+
+            return "E";
+        }
+    }
+}
---
+++ SnakeNetTests/Content/ImageHelperTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using SnakeNet;
+using SnakeNet.Content;
+using Xunit;
+
+namespace SnakeNetTests.Content
+{
+    [Trait("UnitTests", nameof(ImageHelper))]
+    public class ImageHelperTests
+    {
+        /* 
+         * |╔|═|╗|
+         * |║| |║|
+         * |╚|═|╝|
+         */
+
+        [Theory]
+        [InlineData(MoveDirection.Right, MoveDirection.Right, Images.SnakeBodyRight)]
+        [InlineData(MoveDirection.Left, MoveDirection.Left, Images.SnakeBodyLeft)]
+        [InlineData(MoveDirection.Up, MoveDirection.Up, Images.SnakeBodyUp)]
+        [InlineData(MoveDirection.Down, MoveDirection.Down, Images.SnakeBodyDown)]
+        [InlineData(MoveDirection.Left, MoveDirection.Up, Images.SnakeBodyUpRight)]
+        [InlineData(MoveDirection.Left, MoveDirection.Down, Images.SnakeBodyDownRight)]
+        [InlineData(MoveDirection.Right, MoveDirection.Up, Images.SnakeBodyUpLeft)]
+        [InlineData(MoveDirection.Right, MoveDirection.Down, Images.SnakeBodyDownLeft)]
+        [InlineData(MoveDirection.Up, MoveDirection.Left, Images.SnakeBodyDownLeft)]
+        [InlineData(MoveDirection.Up, MoveDirection.Right, Images.SnakeBodyDownRight)]
+        [InlineData(MoveDirection.Down, MoveDirection.Left, Images.SnakeBodyUpLeft)]
+        [InlineData(MoveDirection.Down, MoveDirection.Right, Images.SnakeBodyUpRight)]
+        [InlineData(MoveDirection.Up, MoveDirection.None, Images.SnakeBodyUp)]
+        [InlineData(MoveDirection.Down, MoveDirection.None, Images.SnakeBodyDown)]
+        [InlineData(MoveDirection.Left, MoveDirection.None, Images.SnakeBodyLeft)]
+        [InlineData(MoveDirection.Right, MoveDirection.None, Images.SnakeBodyRight)]
+        public void CalculatesImageCorrectly(MoveDirection previous, MoveDirection next, string expectedImage)
+        {
+            var image = ImageHelper.GetImage(previous, next);
+
+            image.Should().Be(expectedImage);
+        }
+    }
+}
