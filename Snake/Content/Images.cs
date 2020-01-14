ed7cd731b9643c5c86130ea97f37739159f84d12
Ahmed Zaman <ahmedrzaman@gmail.com>
2020-01-14 17:41:33 +0000

    Images

---
+++ Snake/Content/Images.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeNet.Content
+{
+    public static class Images
+    {
+        public const string Wall = "█";
+        public const string Food = "⸰";
+
+        public const string SnakeHeadUp = "^";
+        public const string SnakeHeadDown = "v";
+        public const string SnakeHeadRight = ">";
+        public const string SnakeHeadLeft = "<";
+
+        public const string SnakeBodyLeft = "═";
+        public const string SnakeBodyRight = "═";
+        public const string SnakeBodyUp = "║";
+        public const string SnakeBodyDown = "║";
+        public const string SnakeBodyDownRight = "╔";
+        public const string SnakeBodyDownLeft = "╗";
+        public const string SnakeBodyUpRight = "╚";
+        public const string SnakeBodyUpLeft = "╝";
+    }
+}
