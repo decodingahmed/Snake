bef1f64d71ab0a23840d5e5b3850a7c9f80dc11c
Ahmed Zaman <ahmedrzaman@gmail.com>
2020-01-16 09:54:13 +0000

    Moved snake logic to snake class

---
+++ Snake/GameObjects/Snake.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SnakeNet.Framework;
+
+namespace SnakeNet.GameObjects
+{
+    public class Snake
+    {
+        private readonly IList<SnakeBit> _snake;
+
+        public MoveDirection Direction { get; set; } = MoveDirection.Right;
+
+        public Snake(int lengthOfSnake)
+        {
+            _snake = InitSnake(lengthOfSnake);
+        }
+
+        public void Update(TimeSpan elapsed)
+        {
+            for (var i = _snake.Count - 1; i >= 0; i--)
+            {
+                (var previous, var current, var next) = GetSnakeBits(i, _snake);
+
+                // Update head
+                if (i == 0)
+                {
+                    switch (Direction)
+                    {
+                        case MoveDirection.Up:
+                            current.Y -= 1;
+                            break;
+                        case MoveDirection.Down:
+                            current.Y += 1;
+                            break;
+                        case MoveDirection.Left:
+                            current.X -= 1;
+                            break;
+                        case MoveDirection.Right:
+                            current.X += 1;
+                            break;
+                    }
+
+                    current.Direction = Direction;
+                }
+                else // Update body
+                {
+                    current.X = previous.X;
+                    current.Y = previous.Y;
+                    current.Direction = previous.Direction;
+                }
+            }
+        }
+
+        public void Draw(IRenderer renderer)
+        {
+            const string Wall = "█";
+            const string Food = "⸰";
+
+            const string SnakeHeadUp = "^";
+            const string SnakeHeadDown = "v";
+            const string SnakeHeadRight = ">";
+            const string SnakeHeadLeft = "<";
+
+            const string SnakeBodyHorizontal = "═";
+            const string SnakeBodyVertical = "║";
+            const string SnakeBodyDownRight = "╔";
+            const string SnakeBodyDownLeft = "╗";
+            const string SnakeBodyUpRight = "╚";
+            const string SnakeBodyUpLeft = "╝";
+
+            for (var i = 0; i < _snake.Count; i++)
+            {
+                (var previous, var current, var next) = GetSnakeBits(i, _snake);
+                var bodyCharacter = "";
+
+                // Draw head
+                if (previous == null)
+                {
+                    switch (Direction)
+                    {
+                        case MoveDirection.Down:
+                            bodyCharacter = SnakeHeadDown;
+                            break;
+                        case MoveDirection.Left:
+                            bodyCharacter = SnakeHeadLeft;
+                            break;
+                        case MoveDirection.Right:
+                            bodyCharacter = SnakeHeadRight;
+                            break;
+                        case MoveDirection.Up:
+                            bodyCharacter = SnakeHeadUp;
+                            break;
+                    }
+                }
+                else // Draw body
+                {
+                    switch (current.Direction)
+                    {
+                        case MoveDirection.Down:
+                            bodyCharacter = SnakeHeadDown;
+                            break;
+                        case MoveDirection.Left:
+                            bodyCharacter = SnakeHeadLeft;
+                            break;
+                        case MoveDirection.Right:
+                            bodyCharacter = SnakeHeadRight;
+                            break;
+                        case MoveDirection.Up:
+                            bodyCharacter = SnakeHeadUp;
+                            break;
+                    }
+                }
+
+                renderer.DrawText(bodyCharacter, current.X, current.Y);
+            }
+        }
+
+        private IList<SnakeBit> InitSnake(int lengthOfSnake)
+        {
+            return Enumerable.Range(0, lengthOfSnake)
+                .Reverse()
+                .Select(index => new SnakeBit
+                {
+                    X = index,
+                    Y = 0,
+                    IsHead = index == 0,
+                    Direction = MoveDirection.Right
+                })
+                .ToList();
+        }
+
+        private (SnakeBit previous, SnakeBit current, SnakeBit next) GetSnakeBits(int index, IList<SnakeBit> snake)
+        {
+            return (previous: index == 0 ? null : snake[index - 1],
+                current: snake[index],
+                next: index == snake.Count - 1 ? null : snake[index + 1]);
+        }
+    }
+}
--- Snake/SnakeGame.cs
+++ Snake/SnakeGame.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using SnakeNet.Components;
-using SnakeNet.Content;
-using SnakeNet.Extensions;
 using SnakeNet.Framework;
 using SnakeNet.Framework.Renderer;
 using SnakeNet.GameObjects;
@@ -12,16 +9,14 @@ namespace SnakeNet
 {
     public class SnakeGame : Game
     {
-        private readonly IList<SnakeBit> _snake;
-
-        private MoveDirection _moveDirection = MoveDirection.Right;
+        private readonly Snake _snake;
         
         public SnakeGame()
-            : base(new ConsoleRenderer())
+            : base(new ConsoleRenderer(60, 30))
         {
-            _snake = InitSnake(5);
+            _snake = new Snake(5);
 
-            SetTargetFramesPerSecond(1); // Important for the game to work
+            SetTargetFramesPerSecond(2); // Important for the game to work
         }
 
         public override void HandleInput(TimeSpan elapsed)
@@ -31,143 +26,28 @@ namespace SnakeNet
                 var key = Console.ReadKey().Key;
 
                 if (key == ConsoleKey.UpArrow)
-                    _moveDirection = MoveDirection.Up;
+                    _snake.Direction = MoveDirection.Up;
                 else if (key == ConsoleKey.DownArrow)
-                    _moveDirection = MoveDirection.Down;
+                    _snake.Direction = MoveDirection.Down;
                 else if (key == ConsoleKey.LeftArrow)
-                    _moveDirection = MoveDirection.Left;
+                    _snake.Direction = MoveDirection.Left;
                 else if (key == ConsoleKey.RightArrow)
-                    _moveDirection = MoveDirection.Right;
+                    _snake.Direction = MoveDirection.Right;
             }
         }
 
         public override void Update(TimeSpan elapsed)
         {
             base.Update(elapsed);
-            
-            UpdateSnake(elapsed);
+
+            _snake.Update(elapsed);
         }
 
         public override void Draw(TimeSpan elapsed)
         {
             base.Draw(elapsed);
-            
-            DrawSnake();
-        }
 
-        private IList<SnakeBit> InitSnake(int lengthOfSnake)
-        {
-            return Enumerable.Range(0, lengthOfSnake)
-                .Reverse()
-                .Select(index => new SnakeBit { X = index, Y = 0, IsHead = index == 0, Direction = MoveDirection.Right })
-                .ToList();
-        }
-
-        private void UpdateSnake(TimeSpan elapsed)
-        {
-            for (var i = _snake.Count - 1; i >= 0; i--)
-            {
-                (var previous, var current, var next) = GetSnakeBits(i, _snake);
-
-                // Update head
-                if (i == 0)
-                {
-                    switch (_moveDirection)
-                    {
-                        case MoveDirection.Up:
-                            current.Y -= 1;
-                            break;
-                        case MoveDirection.Down:
-                            current.Y += 1;
-                            break;
-                        case MoveDirection.Left:
-                            current.X -= 1;
-                            break;
-                        case MoveDirection.Right:
-                            current.X += 1;
-                            break;
-                    }
-
-                    current.Direction = _moveDirection;
-                }
-                else // Update body
-                {
-                    current.X = previous.X;
-                    current.Y = previous.Y;
-                    current.Direction = previous.Direction;
-                }
-            }
-        }
-
-        private void DrawSnake()
-        {
-            const string Wall = "█";
-            const string Food = "⸰";
-
-            const string SnakeHeadUp = "^";
-            const string SnakeHeadDown = "v";
-            const string SnakeHeadRight = ">";
-            const string SnakeHeadLeft = "<";
-                
-            const string SnakeBodyHorizontal = "═";
-            const string SnakeBodyVertical = "║";
-            const string SnakeBodyDownRight = "╔";
-            const string SnakeBodyDownLeft = "╗";
-            const string SnakeBodyUpRight = "╚";
-            const string SnakeBodyUpLeft = "╝";
-            
-            for (var i = 0; i < _snake.Count; i++)
-            {
-                (var previous, var current, var next) = GetSnakeBits(i, _snake);
-                var bodyCharacter = "";
-                
-                // Draw head
-                if (previous == null)
-                {
-                    switch (_moveDirection)
-                    {
-                        case MoveDirection.Down:
-                            bodyCharacter = SnakeHeadDown;
-                            break;
-                        case MoveDirection.Left:
-                            bodyCharacter = SnakeHeadLeft;
-                            break;
-                        case MoveDirection.Right:
-                            bodyCharacter = SnakeHeadRight;
-                            break;
-                        case MoveDirection.Up:
-                            bodyCharacter = SnakeHeadUp;
-                            break;
-                    }
-                }
-                else // Draw body
-                {
-                    switch (current.Direction)
-                    {
-                        case MoveDirection.Down:
-                            bodyCharacter = SnakeHeadDown;
-                            break;
-                        case MoveDirection.Left:
-                            bodyCharacter = SnakeHeadLeft;
-                            break;
-                        case MoveDirection.Right:
-                            bodyCharacter = SnakeHeadRight;
-                            break;
-                        case MoveDirection.Up:
-                            bodyCharacter = SnakeHeadUp;
-                            break;
-                    }
-                }
-
-                _renderer.DrawText(bodyCharacter, current.X, current.Y);
-            }
-        }
-
-        private (SnakeBit previous, SnakeBit current, SnakeBit next) GetSnakeBits(int index, IList<SnakeBit> snake)
-        {
-            return (previous: index == 0 ? null : snake[index - 1],
-                current: snake[index],
-                next: index == snake.Count - 1 ? null : snake[index + 1]);
+            _snake.Draw(GameRenderer);
         }
     }
 }
