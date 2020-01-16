afd378a9df135257f39bf99779a124b6813b3702
Ahmed Zaman <ahmedrzaman@gmail.com>
2020-01-16 19:23:51 +0000

    Created a Food class

---
+++ Snake/GameObjects/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SnakeNet.Content;
+using SnakeNet.Framework;
+
+namespace SnakeNet.GameObjects
+{
+    public class Food
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Food(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Draw(IRenderer renderer)
+        {
+            renderer.DrawText(Images.Food, X, Y);
+        }
+    }
+}
